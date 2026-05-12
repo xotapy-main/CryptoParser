5c281d33aab4f1123380d8a1d41481633bc26899
Rebrik Dmitry <rebrik_dmitry@inbox.ru>
2026-05-12 18:48:28 +0700

    Initialed project

---
+++ CryptoParser/src/main/java/com/example/CryptoParser/CryptoParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.CryptoParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoParserApplication.class, args);
+	}
+
+}
---
+++ CryptoParser/src/test/java/com/example/CryptoParser/CryptoParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CryptoParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
